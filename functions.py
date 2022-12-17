d8371c73df3ecd99eda46631821504feba0c5481
Zethyst <zethyst@protonmail.com>
2022-12-18 00:52:01 +0530

    Inital Commit

---
+++ functions.py
@@ -0,0 +1,12 @@
+def Buzz(num):
+    if (num % 7 == 0) or (num % 10 == 7):
+        print("Number is a Buzz Number.")
+    else:
+        print("Number is NOT a Buzz Number")
+
+
+a = int(input("Enter a number: "))
+Buzz(a)
+
+def Func1():
+    pass
